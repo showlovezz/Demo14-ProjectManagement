28bbac0178fa8c6326a4031f7d08dae8a57069e0
vita <dragic.hsieh@gmail.com>
2018-05-23 15:57:58 +0800

    Done add show action、page and modify create action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+  before_action :find_project, only: [:show]
+
   def index
   	@projects = Project.all
   end
@@ -15,13 +17,21 @@ class ProjectsController < ApplicationController
   		redirect_to projects_path
   	else
   		flash[:alert] = "建立不成功"
+      render 'new'
   	end
+  end
+
+  def show
   end
 
   private
 
   def params_project
   	params.require(:project).permit(:title, :description)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
   end
 
 end
