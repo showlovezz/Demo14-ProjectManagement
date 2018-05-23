9f6497be20f75c265c7e4830e904aa0bb4ad735f
vita <dragic.hsieh@gmail.com>
2018-05-23 15:51:46 +0800

    Done add new、create action、new page and modify index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,25 @@ class ProjectsController < ApplicationController
   def index
   	@projects = Project.all
   end
+
+  def new
+  	@project = Project.new
+  end
+
+  def create
+  	@project = Project.new(params_project)
+  	if @project.save
+  		flash[:notice] = "建立成功"
+  		redirect_to projects_path
+  	else
+  		flash[:alert] = "建立不成功"
+  	end
+  end
+
+  private
+
+  def params_project
+  	params.require(:project).permit(:title, :description)
+  end
+
 end
