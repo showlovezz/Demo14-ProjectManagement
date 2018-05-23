2c4cc3ae5e48e05f5679db8b11a1ff2601686c67
vita <dragic.hsieh@gmail.com>
2018-05-23 17:05:58 +0800

    Done add create action and db:migrate for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+
+  before_action :find_project
+
+  def create
+    @task = @project.task.create(params_task)
+    redirect_to @project
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def params_task
+    params.require(:task).permit(:content)
+  end
+
 end
