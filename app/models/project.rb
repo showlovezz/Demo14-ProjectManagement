7a2e8556b0c520160e28fba0e934495b0c66c996
vita <dragic.hsieh@gmail.com>
2018-05-24 11:35:21 +0800

    Done add index page css style and modify project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@ class Project < ApplicationRecord
 	# 驗證 title 及 content 欄位必填
 	validates :title, :description, presence: true
 
-	has_many :tasks
+	# 如果 Project 已經有了 tasks，就不允許刪除（刪除時拋出 Error）
+	has_many :tasks, dependent: :restrict_with_error
 	
 end
